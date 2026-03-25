128fab99c399400155271fa87d42be08b3cebf5b
PurpleRaven05 <81098655+JoseIgnacioValverde@users.noreply.github.com>
2026-03-25 10:38:51 +0100

    Update CameraControl.cs

--- Thumus/Scripts/CameraControl.cs
+++ Thumus/Scripts/CameraControl.cs
@@ -12,12 +12,12 @@ public class CameraControl : MonoBehaviour
     
     void Start()
     {
-        //offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;
     }
 
     
     void LateUpdate()
     {
-        //transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset;
     }
 }
